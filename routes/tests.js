53d9a9721fd3a12b7b36127ed10e1991b45bb42c
Abraham E. Tavarez <abeincwireless@gmail.com>
2024-02-03 12:55:01 -0500

    test endpoitn

--- routes/tests.js
+++ routes/tests.js
@@ -1,16 +1,24 @@
-import {Router} from 'express';
+import { Router } from "express";
 
 const router = new Router();
 
 /**
  * POST /signin
  */
-router.get('/', async (req, res) => {
-    res.send(`
+router.get("/", async (req, res) => {
+  res.send(`
     <div>
         <h2>You got Data!</h2>
     </div>
-    `)
+    `);
+});
+
+router.post("/", (req, res) => {
+  res.send(`
+    <div>
+    <h2>You said: ${res.body}</h2>
+</div>
+    `);
 });
 
 export default router;
